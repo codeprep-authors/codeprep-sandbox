7a39ce12159e1fb99a32d31d40369223494b42f2
shunjikonishi <swandouble@gmail.com>
2017-07-03 17:13:04 +0900

    Add comment

--- src/chat.js
+++ src/chat.js
@@ -1,22 +1,45 @@
 "use strict";
 
+const WebSocket = require('ws');
+
+// 接続中のWebSocketコネクションを保持する配列
 let connections = [];
+
+// 接続したWebSocketに割り振るシーケンスナンバー
 let id = 0;
 
+/**
+ * JSのオブジェクトをJSON化してWebSocketに送信
+ */
 function send(ws, data) {
-  ws.send(JSON.stringify(data));
+  if (ws.readyState === WebSocket.OPEN) {
+    ws.send(JSON.stringify(data));
+  }
 }
 
 function handleChat(ws, request) {
+  // WebSocketにidを割り振り配列に保存 
   ws.id = ++id;
-  console.log("connect", connections.length);
   connections.push(ws);
+
+  // 最初のメッセージを送信
   send(ws, {
     name: "CODEPREP",
     message: "Hi!, there are " + connections.length + " users in this room."
   });
+
+  // HerokuのWebSocketタイムアウトを避けるため30秒ごとにpingメッセージを送信する
+  // (pingメッセージはクライアント側では無視される)
+  ws.intrervalHandle = setInterval(() => {
+    send(ws, {
+      ping: true
+    });
+  }, 30 * 1000);
+
+  // メッセージ受信時の処理
   ws.on("message", msg => {
     try {
+      // 受信データがJSON形式であり、nameとmessageが設定されている場合はブロードキャストする
       const data = JSON.parse(msg);
       if (data && data.name && data.message) {
         connections.forEach(v => send(v, data));
@@ -25,10 +48,12 @@ function handleChat(ws, request) {
       console.log(e);
     }
   });
+
+  // WebSocket切断時の処理
   ws.on("close", () => {
-    console.log("before close", connections.length, ws.id);
+    // 配列からデータを削除し、インターバル処理をクリアする
     connections = connections.filter(v => v.id !== ws.id);
-    console.log("close", connections.length);
+    clearInterval(ws.intrervalHandle);
   });
 }
 
